960b36f3c3f5db1a6f667e4bebb56324004b853e
zeeth99 <zeeth.max@hotmail.co.nz>
2016-10-09 20:13:53 +1300

    Created empty JTable in StatsPanel

--- voxspell/StatsPanel.java
+++ voxspell/StatsPanel.java
@@ -1,49 +1,20 @@
 package voxspell;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("serial")
 public class StatsPanel extends JScrollPane implements ListSelectionListener {
-	private Stats _stats;
-
-
+	
 	public StatsPanel(Stats stats) {
-//		_stats = stats;
-//		int lineCount = 0;
-//		Scanner scanCount = new Scanner(new File(".history/level"+level+"/stats"));
-//		while (scanCount.hasNextLine()) {
-//			lineCount++;
-//			scanCount.nextLine();
-//		}
-//		scanCount.close();
-//
-//		String[] tableHeadings = {"Word", "Times Mastered", "Times Faulted", "Times Failed"};
-//		String[][] tableContents = new String[lineCount][4];
-//		Scanner sc = new Scanner(new File(".history/level"+level+"/stats"));
-//		for (int i = 0; i < lineCount; i++) {
-//			for (int j = 0; j < 4; j++) {
-//				tableContents[i][j] = sc.next();
-//			}
-//		}
-//		sc.close();
-//
-//		statsTable = new JTable(tableContents, tableHeadings);
-//		statsTable.setShowGrid(true);
-//		statsTable.setEnabled(false);
-//		statsTable.setAutoCreateRowSorter(true);
-//
-//		scrollPane = new JScrollPane(statsTable);
-//		scrollPane.setBounds(0, 0, 440, 290);
-//		add(scrollPane);
+		super();
+		JTable table = new JTable(new DefaultTableModel());
+		table.setAutoCreateRowSorter(true);
+		table.setEnabled(false);
+		this.setViewportView(table);
 	}
 
 	@Override
